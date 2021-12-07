0800309f4271cbcfdf03998f66136854316e720d
Teoyak <39246779+Teoyak@users.noreply.github.com>
2021-12-07 15:30:03 +0100

    (rouge) Definition d'une tache
    Première manipulation des données en python
    
    Ajoute :
    - une fonction qui prend une description et qui retourne une tache (sous forme de liste) avec un ID fixe
    - une fonction d'affichage qui n'affiche pas l'état todo

--- MyProject.py
+++ MyProject.py
@@ -1,5 +1,14 @@
-print('hello world ce monde est fou')
-print('parce que ce monde est fou')
+print('hello world')
 
-print('Thomas devient fou aussi')
+# Une fonction qui me retourne une tache bien formée
+def createTask (description) :
+    Task = [1, description, False]
+    return Task
 
+# affichage d'une tache bien formée sur une ligne
+# n'affiche pas encore l'état todo/done
+def printTask (Task) :
+    print(Task[0] + '[ ]' + description)
+    return 
+
+printTask(createTask('voici une tache'))
